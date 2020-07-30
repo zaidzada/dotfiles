d3b7a8be5d57cc8ac9f8f73835b226aaa3b3dd23
Zaid Kokaja <zkokaja@gmail.com>
2020-07-30 15:06:12 -0400

    use getopts

--- install.sh
+++ install.sh
@@ -5,32 +5,60 @@ set -euo pipefail
 IFS=$'\n\t'
 
 dryrun=
+target=""
 
-# TODO - add options
-# TODO - if this is not the git repo, download, cd, and continue
+# Parse args
+while getopts ":hnt:" opt; do
+  case ${opt} in
+    t )
+      target=$OPTARG
+      if ! [[ -d "$target" ]]; then
+          echo "Target '$target' is not a directory"
+          exit 1
+      fi
+      ;;
+    n )
+      dryrun=echo
+      ;;
+    h )
+      echo "Usage:"
+      echo "    install -n                  Dry run. Print the commands."
+      echo "    install -t target           Only install `target` folder."
+      echo "    install -h                  Display this help message."
+      exit 0
+      ;;
+    \? )
+      echo "Invalid Option: -$OPTARG" 1>&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
 
 # Backup existing files
 if ! [[ -d backups ]]; then
     $dryrun mkdir backups
 fi
 
-for df in `git ls-files`; do
+for df in $(git ls-files "$target"); do
     if [[ -f $HOME/${df#*/} ]]; then
         $dryrun cp $HOME/${df#*/} backups/
     fi
 done
 
 # archive backups
-$dryrun tar czf backups.tgz backups
+$dryrun tar czf "backups-$(date +%Y%m%d_%s).tgz" backups
 $dryrun rm -r backups
+
 
 # Set up symlinks
 
 # Choose which directory, or all
-if [[ $# -eq 1 ]]; then
-    dotfiles=`ls -1A "$1"`
-else
+if [[ -z "$target" ]]; then
     dotfiles=$(find "$PWD" -maxdepth 2 -mindepth 2 -name '.*')
+else
+    dotfiles=`ls -1A "$target"`
 fi
 
 for fn in $dotfiles; do
