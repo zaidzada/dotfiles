d1b50c667c983aac5083f202e668451e52b5ec46
Zaid Kokaja <zkokaja@gmail.com>
2021-01-17 23:22:27 -0500

    fix warnings

--- install.sh
+++ install.sh
@@ -18,12 +18,12 @@ while getopts ":hnt:" opt; do
       fi
       ;;
     n )
-      dryrun=echo
+      dryrun="echo"
       ;;
     h )
       echo "Usage:"
       echo "    install -n                  Dry run. Print the commands."
-      echo "    install -t target           Only install `target` folder."
+      echo "    install -t target           Only install $(target) folder."
       echo "    install -h                  Display this help message."
       exit 0
       ;;
@@ -36,14 +36,18 @@ done
 shift $((OPTIND -1))
 
 
+if [[ -n "$dryrun" ]]; then
+    echo "Just a dry run! No commands will be executed."
+fi
+
 # Backup existing files
 if ! [[ -d backups ]]; then
     $dryrun mkdir backups
 fi
 
-for df in $(git ls-files "$target"); do
+for df in $(git ls-files "${target:-.}"); do
     if [[ -f $HOME/${df#*/} ]]; then
-        $dryrun cp $HOME/${df#*/} backups/
+        $dryrun cp "$HOME/${df#*/}" backups/
     fi
 done
 
@@ -58,9 +62,9 @@ $dryrun rm -r backups
 if [[ -z "$target" ]]; then
     dotfiles=$(find "$PWD" -maxdepth 2 -mindepth 2 -name '.*')
 else
-    dotfiles=`ls -1A "$target"`
+    dotfiles=$(ls -1A "$target")
 fi
 
 for fn in $dotfiles; do
-    $dryrun ln -sf $fn $HOME/$(basename $fn)
+    $dryrun ln -sf "$fn $HOME/$(basename $fn)"
 done
